be8f01f9646207a52e583ed048885890150db7b0
bhargav-yarlagadda <bhargavyarlagadda2003@gmail.com>
2024-09-22 20:26:55 +0530

    feature/created recipe page

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from puddleApplication.views import recipes
 urlpatterns = [
+    path('',recipes),
     path('admin/', admin.site.urls),
 ]
--- puddle/puddleApplication/views.py
+++ puddle/puddleApplication/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from puddleApplication.templates import *
+from puddleApplication.models import Recipe
 # Create your views here.
+def recipes(request):
+    if request.method == "POST":
+        data = request.POST
+        recipe_name = data.get('recipe-name')
+        recipe_desc = data.get('recipe-desc')
+        recipe_image = request.FILES.get('recipe-image')
+        Recipe.objects.create(
+            recipeName=recipe_name,
+            recipeDesc=recipe_desc,
+            recipeImage=recipe_image
+        )
+        return redirect('/')
+    return render(request,"recipes.html")
