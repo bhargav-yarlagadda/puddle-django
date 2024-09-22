2aab326c96d84111d5e249c12d11caf6626d3848
bhargav-yarlagadda <bhargavyarlagadda2003@gmail.com>
2024-09-22 19:14:35 +0530

    created Schema for recipes

--- puddle/puddle/settings.py
+++ puddle/puddle/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+EXTERNAL_APPS=[
+    "puddleApplication"
+]
+INSTALLED_APPS+=EXTERNAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- puddle/puddleApplication/models.py
+++ puddle/puddleApplication/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Recipe(models.Model):
+    recipeName = models.CharField(max_length=50,blank=False)
+    recipeDesc = models.TextField()
+    recipeImage = models.ImageField(upload_to="recipeImages")
