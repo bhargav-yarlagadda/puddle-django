f9ae29d28f7695d823823d795669ddca68283347
bhargav-yarlagadda <bhargavyarlagadda2003@gmail.com>
2024-09-24 17:48:55 +0530

    feature / register

--- puddle/puddleApplication/views.py
+++ puddle/puddleApplication/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from puddleApplication.templates import *
 from puddleApplication.models import Recipe
+from django.contrib.auth.models import User
+from django.contrib import messages 
 # Create your views here.
 def recipes(request):
     if request.method == "POST":
@@ -54,4 +56,24 @@ def login(request):
     return render(request,'login.html')
 
 def register(request):
+    if request.method == 'POST':
+        data = request.POST
+        first_name = data.get('first_name')
+        last_name = data.get('last_name')
+        user_name = data.get('username')
+        password = data.get('password')
+        existing_user = User.objects.filter(username=user_name)
+        if existing_user.exists== True:
+            messages.info(request,'Username already exists! please choose a  diffent username')
+            return redirect('/register')
+        user = User.objects.create(
+            first_name=first_name,
+            last_name=last_name,
+            username=user_name,
+        )
+        user.set_password(password)
+        user.save()
+        return redirect('/login')
+    
+
     return render(request,'register.html')
