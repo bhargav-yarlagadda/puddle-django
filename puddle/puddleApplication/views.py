43686d2065a28b31665493a28b1df207bdef078d
bhargav-yarlagadda <bhargavyarlagadda2003@gmail.com>
2024-09-24 12:09:44 +0530

    feature/completed update feature

--- puddle/puddleApplication/views.py
+++ puddle/puddleApplication/views.py
@@ -27,7 +27,24 @@ def delete_recipe(request,id):
     return redirect('/')     
 
   
-def update_recipe(request,id):
-
+def update_recipe(request, id):
+    query_set = Recipe.objects.get(id=id)
+    if request.method == "POST":
+        data = request.POST
+        recipe_name = data.get('recipe-name')
+        recipe_desc = data.get('recipe-desc')
+        recipe_image = request.FILES.get('recipe-image')
+        
+        if recipe_name:
+            query_set.recipeName = recipe_name
+        if recipe_desc:
+            query_set.recipeDesc = recipe_desc
+        # Update image only if a new file is uploaded
+        if recipe_image:
+            query_set.recipeImage = recipe_image
+            
+        query_set.save()
+        return redirect('/')
     
-    return render(request,'update.html')
+    context = {'recipe': query_set}
+    return render(request, 'update.html', context=context)
